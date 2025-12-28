c9c98d13d4bc9b6479eef7153c04f2f5b921b8c0
RevX05@ <117154752+RavikantShinde05@users.noreply.github.com>
2025-12-28 19:29:17 +0530

    Add files via upload

---
+++ Smart_Contracts/StorageContract.sol
@@ -0,0 +1,20 @@
+//  SPDX-Licence-Identifier: MIT
+
+pragma solidity >=0.8.9;
+
+
+contract Storage{
+
+        int MyNumber;
+
+    function store(int _a) public{
+        MyNumber = _a;
+
+    }
+
+    function read()public view returns(int){
+        return MyNumber;
+
+    }
+
+}
